ea03fb646617ee423e2860ec45530836c022b1d0
devonmcdonald00 <devonmcdonald00@yahoo.com>
2021-08-30 10:34:44 -0500

    config changes

--- src/main/java/com/foodbox/FoodBox/FoodBoxApplication.java
+++ src/main/java/com/foodbox/FoodBox/FoodBoxApplication.java
@@ -2,9 +2,19 @@ package com.foodbox.FoodBox;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 @SpringBootApplication
 public class FoodBoxApplication {
+	
+	@RequestMapping("/")
+    @ResponseBody
+    String home() {
+      return "Hello World!";
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(FoodBoxApplication.class, args);
